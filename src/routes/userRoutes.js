698745fa66d1193f4538df9eb25ea983a367d03e
lico <lico4268@gmail.com>
2024-12-02 01:57:57 +0900

    fix final

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -18,12 +18,11 @@ app.post('/update',async(req,res)=>{
             return res.status(404).json({ message: '해당 사용자를 찾을 수 없습니다.' });
           }
 
+        conn.release();
         res.status(200).json({ message: '프로필이 성공적으로 업데이트되었습니다.' });
     } catch (err){
         console.log(err);
         res.status(500).json({ message: '서버 오류가 발생했습니다.' });    
-    } finally{
-        if (conn) conn.release();
     }
 
 
